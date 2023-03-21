095259a418ceec43f07707af3c7c3119b9e25be1
Zahrashareef <91188380+Zahrashareef@users.noreply.github.com>
2023-03-21 01:56:30 +0000

    updated app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,38 +1,65 @@
-// import preact
 import { h, Component } from 'preact';
-
-// import required Components from 'components/'
 import Iphone from './iphone';
 import Ipad from './ipad';
 
 export default class App extends Component {
 	constructor(props) {
-	  super(props);
-	  this.state = {
-		isTablet: false,
-		weatherData: null
-	  };
-	  this.handleWeatherData = this.handleWeatherData.bind(this);
+		super(props);
+		this.state = {
+			isTablet: false,
+			latitude: null,
+			longitude: null,
+			error: null
+		};
+		this.handleWeatherData = this.handleWeatherData.bind(this);
 	}
-  
+
+	componentDidMount() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				position => {
+					this.setState({
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude
+					});
+				},
+				error => {
+					this.setState({ error: error.message });
+				}
+			);
+		} else {
+			this.setState({ error: 'Geolocation is not supported by your browser.' });
+		}
+	}
+
 	handleWeatherData(data) {
-	  this.setState({ weatherData: data });
+		this.setState({ weatherData: data });
 	}
-  
+
 	render() {
-	  if (this.state.isTablet) {
-		return (
-		  <div id="app">
-			<Ipad />
-		  </div>
-		);
-	  } else {
-		return (
-		  <div id="app">
-			<Iphone onWeatherData={this.handleWeatherData} />
-		  </div>
-		);
-	  }
+		const { isTablet, latitude, longitude, error, weatherData } = this.state;
+
+		if (error) {
+			return (
+				<div id="app">
+					<p>{error}</p>
+				</div>
+			);
+		}
+
+		if (isTablet) {
+			return (
+				<div id="app">
+					<Ipad />
+				</div>
+			);
+		} else {
+			return (
+				<div id="app">
+					{latitude && longitude && <Iphone onWeatherData={this.handleWeatherData} latitude={latitude} longitude={longitude} />}
+					{weatherData && <p>Weather Data: {JSON.stringify(weatherData)}</p>}
+				</div>
+			);
+		}
 	}
-  }
-  
+}
