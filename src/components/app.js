f0673d617940fb9c0a3a73aed7a8546767c60fa8
mayarsim <98826563+mayarsim@users.noreply.github.com>
2023-03-24 04:04:50 +0000

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -6,39 +6,32 @@ import Iphone from './iphone';
 import Ipad from './ipad';
 
 export default class App extends Component {
-//var App = React.createClass({
-
-	// once the components are loaded, checks if the url bar has a path with "ipad" in it, if so sets state of tablet to be true
-	componentDidMount() {
-		const urlBar = window.location.href;
-		if(urlBar.includes("ipad")) {
-			this.setState({
-				"isTablet": true
-			});
-		} else {
-			this.setState({
-				"isTablet": false
-			});
-		}
+	constructor(props) {
+	  super(props);
+	  this.state = {
+		isTablet: false,
+		weatherData: null
+	  };
+	  this.handleWeatherData = this.handleWeatherData.bind(this);
 	}
-
-	/*
-		A render method to display the required Component on screen (iPhone or iPad) : selected by checking component's isTablet state
-	*/
-	render(){
-		if(this.state.isTablet){
-			return (
-				<div id="app">
-					<Ipad/ >
-				</div>   				
-			);
-		} 
-		else {
-			return (
-				<div id="app">
-					<Iphone/ >
-				</div>
-			);
-		}
+  
+	handleWeatherData(data) {
+	  this.setState({ weatherData: data });
 	}
-}
+  
+	render() {
+	  if (this.state.isTablet) {
+		return (
+		  <div id="app">
+			<Ipad />
+		  </div>
+		);
+	  } else {
+		return (
+		  <div id="app">
+			<Iphone onWeatherData={this.handleWeatherData} />
+		  </div>
+		);
+	  }
+	}
+  }
