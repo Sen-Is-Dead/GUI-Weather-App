73ad120f93328d9ba20ea60cf25d5b15f2baab35
Zahrashareef <91188380+Zahrashareef@users.noreply.github.com>
2023-03-24 00:20:48 +0000

    comments

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,10 @@ import { h, Component } from 'preact';
 import Iphone from './iphone';
 import Ipad from './ipad';
 
+// Main App component
 export default class App extends Component {
+
+// Initialize state and bind methods
 	constructor(props) {
 	  super(props);
 	  this.state = {
@@ -14,19 +17,25 @@ export default class App extends Component {
 	  };
 	  this.handleWeatherData = this.handleWeatherData.bind(this);
 	}
-  
+
+// Method to handle weather data received from child components
 	handleWeatherData(data) {
 	  this.setState({ weatherData: data });
 	}
-  
+
+// Render method for the component
 	render() {
+
+// Check if the device is a tablet or not
 	  if (this.state.isTablet) {
-		return (
+// Render the Ipad component
+		  return (
 		  <div id="app">
 			<Ipad />
 		  </div>
 		);
 	  } else {
+// Render the Ipad component
 		return (
 		  <div id="app">
 			<Iphone onWeatherData={this.handleWeatherData} />
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,16 +1,23 @@
 // import preact
 import { h, render, Component } from 'preact';
-	
+
+// Button component	
 export default class Button extends Component {
 
-	// rendering a function when the button is clicked
+// rendering a function when the button is clicked
 	render() {
+
+// Get the clickFunction prop and check if it's a valid function
 		let cFunction = this.props.clickFunction;
 		if(typeof cFunction !== 'function'){
+
+// If it's not a function, log an error message and set a default function
 			cFunction = () => {
 				console.log("passed something as 'clickFunction' that wasn't a function !");
 			}
 		}
+
+// Return the button element with an onClick event that calls the clickFunction
 		return (
 			<div>
 				<button onClick={cFunction}>
